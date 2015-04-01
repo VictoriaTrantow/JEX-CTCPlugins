2f44905bb78b78efd4f88995b72eacb3b9ac6493
jaywarrick <jaywarrick@dyn-72-33-209-52.uwnet.wisc.edu>
2015-04-01 13:43:32 -0500

    Convert to JEXPlugin

--- src/main/java/plugins/CTC_JEX_CTC_SingleCellQuantification.java
+++ src/main/java/plugins/CTC_JEX_CTC_SingleCellQuantification.java
@@ -1,14 +1,5 @@
 package plugins;
 
-import Database.DBObjects.JEXData;
-import Database.DBObjects.JEXEntry;
-import Database.DataReader.ImageReader;
-import Database.DataReader.RoiReader;
-import Database.DataWriter.FileWriter;
-import Database.Definition.ParameterSet;
-import Database.Definition.Type;
-import Database.Definition.TypeName;
-import function.JEXCrunchable;
 import ij.ImagePlus;
 import ij.gui.PolygonRoi;
 import ij.gui.Roi;
@@ -30,211 +21,90 @@ import java.util.Vector;
 import jex.statics.JEXStatics;
 import miscellaneous.Canceler;
 import miscellaneous.StatisticsUtility;
+
+import org.scijava.plugin.Plugin;
+
 import tables.DimTable;
 import tables.DimensionMap;
 import weka.core.converters.JEXTableWriter;
+import Database.DBObjects.JEXData;
+import Database.DBObjects.JEXEntry;
+import Database.DataReader.ImageReader;
+import Database.DataReader.RoiReader;
+import Database.DataWriter.FileWriter;
+import Database.Definition.Type;
+import function.plugin.mechanism.InputMarker;
+import function.plugin.mechanism.JEXPlugin;
+import function.plugin.mechanism.MarkerConstants;
+import function.plugin.mechanism.OutputMarker;
 
-/**
- * This is a JEXperiment function template To use it follow the following instructions
- * 
- * 1. Fill in all the required methods according to their specific instructions 2. Place the file in the Functions/SingleDataPointFunctions folder 3. Compile and run JEX!
- * 
- * JEX enables the use of several data object types The specific API for these can be found in the main JEXperiment folder. These API provide methods to retrieve data from these objects, create new objects and handle the data they contain.
- * 
- * @author erwinberthier
- * 
- */
-public class CTC_JEX_CTC_SingleCellQuantification extends JEXCrunchable {
-	
-	public CTC_JEX_CTC_SingleCellQuantification()
-	{}
-	
-	// ----------------------------------------------------
-	// --------- INFORMATION ABOUT THE FUNCTION -----------
-	// ----------------------------------------------------
-	
-	/**
-	 * Returns the name of the function
-	 * 
-	 * @return Name string
-	 */
-	@Override
-	public String getName()
-	{
-		String result = "CTC Single Cell Quantification";
-		return result;
-	}
-	
-	/**
-	 * This method returns a string explaining what this method does This is purely informational and will display in JEX
-	 * 
-	 * @return Information string
-	 */
-	@Override
-	public String getInfo()
-	{
-		String result = "Use the cropped points images to quantify cell attributes.";
-		return result;
-	}
-	
-	/**
-	 * This method defines in which group of function this function will be shown in... Toolboxes (choose one, caps matter): Visualization, Image processing, Custom Cell Analysis, Cell tracking, Image tools Stack processing, Data Importing, Custom
-	 * image analysis, Matlab/Octave
-	 * 
-	 */
-	@Override
-	public String getToolbox()
-	{
-		String toolbox = "CTC Toolbox";
-		return toolbox;
-	}
-	
-	/**
-	 * This method defines if the function appears in the list in JEX It should be set to true expect if you have good reason for it
-	 * 
-	 * @return true if function shows in JEX
-	 */
-	@Override
-	public boolean showInList()
-	{
-		return true;
-	}
-	
-	/**
-	 * Returns true if the user wants to allow multithreding
-	 * 
-	 * @return
-	 */
-	@Override
-	public boolean allowMultithreading()
-	{
-		return true;
-	}
-	
-	// ----------------------------------------------------
-	// --------- INPUT OUTPUT DEFINITIONS -----------------
-	// ----------------------------------------------------
-	
-	/**
-	 * Return the array of input names
-	 * 
-	 * @return array of input names
-	 */
-	@Override
-	public TypeName[] getInputNames()
-	{
-		TypeName[] inputNames = new TypeName[4];
-		inputNames[0] = new TypeName(ROI, "Maxima");
-		inputNames[1] = new TypeName(IMAGE, "Segmented Image");
-		inputNames[2] = new TypeName(IMAGE, "Mask Image");
-		inputNames[3] = new TypeName(IMAGE, "Image To Quantify");
-		return inputNames;
-	}
-	
-	/**
-	 * Return the array of output names defined for this function
-	 * 
-	 * @return
-	 */
-	@Override
-	public TypeName[] getOutputs()
-	{
-		this.defaultOutputNames = new TypeName[1];
-		this.defaultOutputNames[0] = new TypeName(FILE, "Cell Measurements");
-		
-		if(this.outputNames == null)
-		{
-			return this.defaultOutputNames;
-		}
-		return this.outputNames;
-	}
-	
-	/**
-	 * Returns a list of parameters necessary for this function to run... Every parameter is defined as a line in a form that provides the ability to set how it will be displayed to the user and what options are available to choose from The simplest
-	 * FormLine can be written as: FormLine p = new FormLine(parameterName); This will provide a text field for the user to input the value of the parameter named parameterName More complex displaying options can be set by consulting the FormLine API
-	 * 
-	 * @return list of FormLine to create a parameter panel
-	 */
-	@Override
-	public ParameterSet requiredParameters()
-	{
-		// (ImageProcessor ip, double tolerance, double threshold, int
-		// outputType, boolean excludeOnEdges, boolean isEDM, Roi roiArg,
-		// boolean lightBackground)
-		// Parameter p0 = new
-		// Parameter("Dummy Parameter","Lets user know that the function has been selected.",FormLine.DROPDOWN,new
-		// String[] {"true"},0);
-		// Parameter pa1 = new Parameter("Color Dim Name", "Name of the color dimension.", "Color");
-		// Parameter pa2 = new Parameter("Dummy Parameter", "The type of measurement to perform", Parameter.DROPDOWN, operations, 0);
-		
-		// Make an array of the parameters and return it
-		ParameterSet parameterArray = new ParameterSet();
-		// parameterArray.addParameter(p0);
-		// parameterArray.addParameter(pa1);
-		// parameterArray.addParameter(pa2);
-		
-		return parameterArray;
-	}
+
+@Plugin(
+		type = JEXPlugin.class,
+		name="CTC - Single Cell Quantification",
+		menuPath="CTC Toolbox",
+		visible=true,
+		description="Quantify single-cell attributes from a point roi, mask overlay, segmentation overlay, and image."
+		)
+public class CTC_JEX_CTC_SingleCellQuantification extends JEXPlugin {
 	
 	public static String[] binaryMeasures = new String[] { "AREA", "PERIMETER", "CIRCULARITY" };
 	public static String[] grayscaleMeasures = new String[] { "MEAN", "MEDIAN", "MIN", "MAX", "SUM", "STDDEV", "VARIANCE" };
 	
-	// ----------------------------------------------------
-	// --------- ERROR CHECKING METHODS -------------------
-	// ----------------------------------------------------
 	
-	/**
-	 * Returns the status of the input validity checking It is HIGHLY recommended to implement input checking however this can be over-ridden by returning false If over-ridden ANY batch function using this function will not be able perform error
-	 * checking...
-	 * 
-	 * @return true if input checking is on
-	 */
-	@Override
-	public boolean isInputValidityCheckingEnabled()
+	public CTC_JEX_CTC_SingleCellQuantification()
+	{}
+	
+	/////////// Define Inputs ///////////
+	
+	@InputMarker(name="Maxima", type=MarkerConstants.TYPE_ROI, description="Maxima that define cell locations/id's", optional=false)
+	JEXData maximaData;
+	
+	@InputMarker(name="Segmented Image", type=MarkerConstants.TYPE_IMAGE, description="Segmentation image", optional=false)
+	JEXData segData;
+	
+	@InputMarker(name="Mask Image", type=MarkerConstants.TYPE_IMAGE, description="Black and white (i.e., binary) image that defines cell regions to be used in conjunction with the maxima defining cell locations.", optional=false)
+	JEXData maskData;
+	
+	@InputMarker(name="Image To Quantify", type=MarkerConstants.TYPE_IMAGE, description="Image to be quantified.", optional=false)
+	JEXData imageData;
+	
+	/////////// Define Parameters ///////////
+	
+	// No parameters necessary
+	
+	/////////// Define Outputs ///////////
+	
+	@OutputMarker(name="Cell Measurements", type=MarkerConstants.TYPE_FILE, flavor="", description="The single-cell measurements", enabled=true)
+	JEXData output;
+
+	public int getMaxThreads()
 	{
-		return false;
+		return 10;
 	}
 	
-	// ----------------------------------------------------
-	// --------- THE ACTUAL MEAT OF THIS FUNCTION ---------
-	// ----------------------------------------------------
-	
-	/**
-	 * Perform the algorithm here
-	 * 
-	 */
 	@Override
-	public boolean run(JEXEntry entry, HashMap<String,JEXData> inputs)
+	public boolean run(JEXEntry entry)
 	{
 		try
 		{
-			/*
-			 * inputNames[2] = new TypeName(IMAGE, "Outer Boundary Image"); inputNames[3] = new TypeName(IMAGE, "Inner Boundary Image"); inputNames[4] = new TypeName(IMAGE, "Image To Quantify");
-			 */
 			// Collect the inputs
-			JEXData maximaData = getInputAs(inputs, "Maxima", ROI);
-			if(maximaData == null)
+			if(maximaData == null && maximaData.getTypeName().getType().matches(JEXData.ROI))
 			{
 				return false;
 			}
-			JEXData segData = getInputAs(inputs, "Segmented Image", IMAGE);
-			if(segData == null)
+			if(segData == null && segData.getTypeName().getType().matches(JEXData.IMAGE))
 			{
 				return false;
 			}
-			JEXData maskData = getInputAs(inputs, "Mask Image", IMAGE);
-			if(maskData == null)
+			if(maskData == null && maskData.getTypeName().getType().matches(JEXData.IMAGE))
 			{
 				return false;
 			}
-			JEXData imageData = getInputAs(inputs, "Image To Quantify", IMAGE);
-			if(imageData == null)
+			if(imageData == null && imageData.getTypeName().getType().matches(JEXData.IMAGE))
 			{
 				return false;
 			}
-			
-			// Gather parameters
-			// String op = this.parameters.getValueOfParameter("Measurement");
 			
 			DimTable imageTable = imageData.getDimTable();
 			
@@ -258,11 +128,8 @@ public class CTC_JEX_CTC_SingleCellQuantification extends JEXCrunchable {
 				JEXStatics.statusBar.setProgressPercentage((int) (100 * count / total));
 			}
 			
-			String resultsFile = JEXTableWriter.writeTable(this.outputNames[0].getName(), results);
-			JEXData output0 = FileWriter.makeFileObject(this.outputNames[0].getName(),null, resultsFile);
-			
-			// Set the outputs
-			this.realOutputs.add(output0);
+			String resultsFile = JEXTableWriter.writeTable("SingleCellData", results);
+			output = FileWriter.makeFileObject("temp", null, resultsFile);
 			
 			// Return status
 			return true;
